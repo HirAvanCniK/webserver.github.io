9bf4f469bd18a70a6232c5dfcc2c6166b7113704
HirAvanCniK <vanniandrea244466666@gmail.com>
2024-03-08 22:46:10 +0100

    Update script3.js

--- static/js/script3.js
+++ static/js/script3.js
@@ -31,23 +31,26 @@ if(Istatus == "on"){
     btn.style.boxShadow = "inset 0 0 50px 1px #ee3530";
     btn_text.textContent = "Stop Instance";
     btn_icon.classList.add("fa-stop");
+    message.textContent = "The terminal instance is running.";
+    message.style.color = "rgba(255, 0, 0, 0.6)";
 }else if(Istatus == "off"){
     hoverElement(btn, "inset 0 0 30px 1px springgreen", "inset 0 0 50px 1px springgreen");
     btn.style.boxShadow = "inset 0 0 50px 1px springgreen";
     btn_text.textContent = "Start Instance";
     btn_icon.classList.add("fa-play");
     message.textContent = "The terminal instance is not running.";
+    message.style.color = "gold";
 }else if(Istatus == "error"){
     hoverElement(btn, "inset 0 0 30px 1px springgreen", "inset 0 0 50px 1px springgreen");
     btn.style.boxShadow = "inset 0 0 50px 1px springgreen";
     btn_text.textContent = "Start Instance";
     btn_icon.classList.add("fa-play");
     message.textContent = "There was an error in generating the terminal instance.";
+    message.style.color = "gold";
 }
 
 btn.appendChild(btn_icon);
 btn.appendChild(btn_text);
-message.style.color = "gold";
 message.style.fontWeight = "bold";
 let terminalMsg = document.getElementById("terminalmsg");
 terminalMsg.appendChild(message);
