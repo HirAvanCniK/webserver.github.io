c9eb50b7701549923870e72ca411d73dfef2af33
HirAvanCniK <93336162+HirAvanCniK@users.noreply.github.com>
2024-03-13 16:45:19 +0100

    Update sync_webserver.sh

--- sync_webserver.sh
+++ sync_webserver.sh
@@ -4,26 +4,35 @@ DESTINATION="/var/www/webserver.irvanni.it"
 USERNAME="HirAvanCniK"
 REPOSITORY="webserver.github.io"
 
-cd $DESTINATION
-rm -rf * .git .gitignore
+function clone(){
+  cd $DESTINATION
+  rm -rf * .git .gitignore
 
-git clone https://github.com/$USERNAME/$REPOSITORY.git $DESTINATION
-npm install
-chmod -R 777 $DESTINATION
+  git clone https://github.com/$USERNAME/$REPOSITORY.git $DESTINATION
+  npm install
+  chmod -R 777 $DESTINATION
+}
 
 function check_udates() {
   SHA1_ONLINE=$(git ls-remote https://github.com/$USERNAME/$REPOSITORY.git HEAD | awk '{print $1}')
   SHA1_LOCAL=$(git rev-parse HEAD)
   exit_code=$(echo $?)
-  if [ "$SHA1_ONLINE" != "$SHA1_LOCAL" -o $exit_code != 0 ]; then
+  if [ "$SHA1_ONLINE" != "$SHA1_LOCAL" -o "$exit_code" != "0" ]; then
     echo "Updates available..."
-    git pull --force origin main
-    npm install
-    chmod -R 777 $DESTINATION
-  else
+    git pull origin main
+    $exit_code=$(echo $?)
+    if [ "$exit_code" != "0" ]; then
+      clone
+    else
+      npm install
+      chmod -R 777 $DESTINATION
+    fi 
+ else
     echo "Nice there are not updates!"
   fi
 }
+
+clone
 
 while true; do
   check_udates
